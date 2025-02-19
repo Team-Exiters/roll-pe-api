cd1459174f351a22dfd536d6d82519372cd37f34
wogml8524 <wogml8524@naver.com>
2025-02-19 20:06:47 +0900

    유저 이름 및 코드로 검색, 카카오 로그인 ios 처리

--- rollpe/user/urls.py
+++ rollpe/user/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
-from .apis import signup_api, CustomTokenObtainPairAPI, logout_api, VerifyEmailAPI, ForgotPasswordAPI, myid_in_invate_rollpe_api, receiver_is_me_api
+from .apis import signup_api, CustomTokenObtainPairAPI, logout_api, VerifyEmailAPI, ForgotPasswordAPI, myid_in_invate_rollpe_api, receiver_is_me_api, search_user_name
 from .apis_social import social_login, KakaoLoginView
 
 urlpatterns = [
@@ -11,6 +11,9 @@ urlpatterns = [
     path("/verify-email", VerifyEmailAPI.as_view(), name="verify_email"),
     path('/logout', logout_api, name='logout'),
     path('/forgot-password', ForgotPasswordAPI.as_view(), name='forgot_password'),
+
+
+    path('/search-user/', search_user_name, name='search_user_name'),
     path('/papers/inviting-user/', myid_in_invate_rollpe_api, name='myid_in_invate_rollpe'),
     path('/papers/receiver/', receiver_is_me_api, name='receiver_is_me'),
 
