5bdee3e7556a0c6f6c0217b765f587e7ec9ed1e3
Jeongmingz <joungminyt@gmail.com>
2025-02-22 15:59:33 +0900

    feat: User Mypage Paper API

--- rollpe/paper/urls.py
+++ rollpe/paper/urls.py
@@ -2,10 +2,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from paper.apis import UserPaperAPI, PaperAPI, PaperEnterManageAPI
+from paper.apis import UserPaperAPI, PaperAPI, PaperEnterManageAPI, MyPagePaperAPI
 
 urlpatterns = [
 	path('', PaperAPI.as_view(), name='paper_api'),
 	path('/user', UserPaperAPI.as_view(), name='paper_api_for_user'),
+	path('/mypage', MyPagePaperAPI.as_view(), name='user_mypage_paper_api'),
 	path('/enter', PaperEnterManageAPI.as_view(), name='paper_invite_api'),
 ]
