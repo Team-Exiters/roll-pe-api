3fa79fff961e567781cd9fdf90490c8b3a1c27ec
wogml8524 <wogml8524@naver.com>
2025-02-27 16:10:32 +0900

    구글 로그인  구현

--- rollpe/user/urls.py
+++ rollpe/user/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 from .apis import signup_api, CustomTokenObtainPairAPI, logout_api, VerifyEmailAPI, ForgotPasswordAPI, myid_in_invate_rollpe_api, receiver_is_me_api, search_user_name
-from .apis_social import social_login, KakaoLoginView
+from .apis_social import social_login, KakaoLoginView, GoogleLoginView
 
 urlpatterns = [
     # path('signin', TokenObtainPairView.as_view(), name='token_obtain_pair'),
@@ -20,4 +20,5 @@ urlpatterns = [
     # social
     path('/social/login/<str:provider>', social_login, name='social_login'),
     path('/social-kakao', KakaoLoginView.as_view(), name='social_kakao'),
+    path('/social-google', GoogleLoginView.as_view(), name='social_google'),
 ]
