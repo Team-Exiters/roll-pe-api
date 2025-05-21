541bf724aa9d5ac8bd47ad299739116afe6f4c74
wogml8524 <wogml8524@naver.com>
2025-05-21 18:02:18 +0900

    개인정보처리방침, 이용약관 api

--- rollpe/user/urls.py
+++ rollpe/user/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 from .apis import signup_api, CustomTokenObtainPairAPI, logout_api, VerifyEmailAPI, ForgotPasswordAPI, \
-    myid_in_invate_rollpe_api, receiver_is_me_api, search_user_name, user_password_check, change_user_password, drop_user_data
+    myid_in_invate_rollpe_api, receiver_is_me_api, search_user_name, user_password_check, change_user_password, drop_user_data, get_docs_api
 from .apis_social import social_login, KakaoLoginView, GoogleLoginView
 
 urlpatterns = [
@@ -24,4 +24,7 @@ urlpatterns = [
     path('/social/login/<str:provider>', social_login, name='social_login'),
     path('/social-kakao', KakaoLoginView.as_view(), name='social_kakao'),
     path('/social-google', GoogleLoginView.as_view(), name='social_google'),
+
+    # 개인정보처리방침, 이용약관
+    path('/docs',get_docs_api, name='docs')
 ]
